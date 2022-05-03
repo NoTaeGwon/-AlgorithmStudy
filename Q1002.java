625bb3b17112363beda648120b7fcb61b9bc29fd
NoTaeGwon <tg09260926@gmail.com>
2022-05-03 19:55:27 +0900

    study
    study week 1

---
+++ Q1002.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Q1002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int x1, y1, r1, x2, y2, r2;
+		
+		
+
+	}
+
+}
