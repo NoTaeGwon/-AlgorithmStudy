de56ee2fa238797b996536ff2f4592acf1eb635d
NoTaeGwon <103228982+NoTaeGwon@users.noreply.github.com>
2022-10-25 14:42:49 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 356 ms, Memory: 18288 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int starCount;
+		Scanner sc = new Scanner(System.in);
+		starCount = sc.nextInt();
+		
+		for(int x = 1; x <= starCount; x++) {
+			for(int y = starCount; y > x; y--) {
+				System.out.print(" ");
+			}
+			for (int a = 0; a < x; a++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+			}
+		
+
+	}
+
+}
